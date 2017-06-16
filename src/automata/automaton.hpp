3d4175a1f564da6b1fc72e8f519820f06fea3828
Duc Thanh Tran <18ducthanh@gmail.com>
2017-06-16 02:34:32 +0200

    revamp of automaton class - changed namespace as well

--- src/automata/automaton.hpp
+++ src/automata/automaton.hpp
@@ -3,34 +3,20 @@
 #ifndef _AUTOMATON_HPP_
 #define _AUTOMATON_HPP_
 
-#include <boost/dynamic_bitset>
-#include <unordered_map>
-#include <utility>
+#include <state.hpp>
 #include <vector>
 
-namespace Automata {
+namespace Buchidec {
 
-// Represents an automaton. Acts as a base class.
 class Automaton {
 public:
-    virtual void addTransition(const int p, const char a, const int q) = 0;
-
-    inline void addState() { ++m_numberOfStates; }
-
-protected:
-    virtual Automaton(const uint numberOfStates, const uint initialState) : m_numberOfStates(numberOfStates), m_initialState(initialState) {}
-
+    Automaton() = default;
 
 private:
-    int m_numberOfStates;
-    int m_initialState;
-
-    // If bit at i-th position is equal to one, then state i is a final state
-    boost::dynamic_bitset<> m_finalStates;
-
-    std::unordered_multimap<uint, std::pair<char,uint>> m_transitions;
+    std::vector<State> m_states;
+    State m_initialState;
 };
 
-} // namespace Automata
+} // namespace Buchidec
 
 #endif
