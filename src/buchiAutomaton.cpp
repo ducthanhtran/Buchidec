3cbcf7603e34dd3710b6cf4dc9042d20971e471f
Duc Thanh Tran <18ducthanh@gmail.com>
2017-06-17 10:49:16 +0200

    ctor

---
+++ src/buchiAutomaton.cpp
@@ -0,0 +1,9 @@
+// Duc Thanh Transition
+
+#include <buchiAutomaton.hpp>
+
+namespace INWAD {
+
+BuchiAutomaton::BuchiAutomaton(const int numberOfStates) : m_adjacencyList(numberOfStates) {}
+
+} // namespace INWAD
