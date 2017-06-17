c733d0ab62f02fbd816011dad2962bc56e7a8ce3
Duc Thanh Tran <18ducthanh@gmail.com>
2017-06-17 21:34:57 +0200

    automata operations - declarations

--- src/buchiAutomaton.cpp
+++ src/buchiAutomaton.cpp
@@ -6,4 +6,16 @@ namespace INWAD {
 
 BuchiAutomaton::BuchiAutomaton(const int numberOfStates) : m_adjacencyList(numberOfStates) {}
 
+BuchiAutomaton operator|(const BuchiAutomaton &autA, const BuchiAutomaton &autB) {
+    // TODO
+}
+
+BuchiAutomaton operator&(const BuchiAutomaton &autA, const BuchiAutomaton &autB) {
+    // TODO
+}
+
+BuchiAutomaton operator~(const BuchiAutomaton &autA) {
+    // TODO    
+}
+
 } // namespace INWAD
--- src/buchiAutomaton.hpp
+++ src/buchiAutomaton.hpp
@@ -12,12 +12,20 @@ namespace INWAD {
 using State = unsigned long;
 using Transition = std::pair<std::string,State>;
 
+//! A non-deterministic Buchi Automaton.
 class BuchiAutomaton {
 public:
     BuchiAutomaton() = default;
     BuchiAutomaton(const int numberOfStates);
 
     void addEdge(const State source, const std::string &label, const State target);
+    //! Union operation
+    friend BuchiAutomaton operator|(const BuchiAutomaton &autA, const BuchiAutomaton &autB);
+    //! Intersection operation
+    friend BuchiAutomaton operator&(const BuchiAutomaton &autB, const BuchiAutomaton &autB);
+    //! Complementation operation
+    friend BuchiAutomaton operator~(const BuchiAutomaton &aut);
+
 
 private:
     std::vector<std::vector<Transition>> m_adjacencyList;
