499084f67d1bc1e409ffef6b59e063efbbfb8518
Duc Thanh Tran <18ducthanh@gmail.com>
2017-06-17 10:50:02 +0200

    use unsigned long (at least 32Bytes) for states and std::string as labels between states (for now - prototyping)

---
+++ src/buchiAutomaton.hpp
@@ -0,0 +1,29 @@
+// Author: Duc Thanh Tran
+
+#ifndef _AUTOMATON_HPP_
+#define _AUTOMATON_HPP_
+
+#include <state.hpp>
+#include <tuple>
+#include <vector>
+
+namespace INWAD {
+
+using State = unsigned long;
+using Transition = std::pair<std::string,State>;
+
+class BuchiAutomaton {
+public:
+    BuchiAutomaton() = default;
+    BuchiAutomaton(const int numberOfStates);
+
+    void addEdge(const State source, const std::string &label, const State target);
+
+private:
+    std::vector<std::vector<Transition>> m_adjacencyList;
+    State m_initialState;
+};
+
+} // namespace INWAD
+
+#endif
