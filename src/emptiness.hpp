ba11fbabc97047ccdb28b15d7f410a11674cca60
Duc Thanh Tran <ducthanhtran@users.noreply.github.com>
2017-06-25 23:51:22 +0200

    added emptiness body structure (namespace function)

---
+++ src/emptiness.hpp
@@ -0,0 +1,11 @@
+#ifndef _EMPTINESS_HPP_
+#define _EMPTINESS_HPP_
+
+namespace INWAD {
+    namespace Detail {
+
+    } // namespace Detail
+
+    bool isEmptyLanguage(const BuchiAutomaton &aut);
+} // namespace INWAD
+#endif
