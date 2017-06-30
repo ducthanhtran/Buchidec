1c3d464128f41cba8a8ce56a9a1a0bd9a20d8afa
Duc Thanh Tran <ducthanhtran@users.noreply.github.com>
2017-06-30 07:24:01 +0200

    small test in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,14 @@
+#include <buchiAutomaton.hpp>
 #include <iostream>
+#include <string>
 
-int main(int argc, char* argv[]) {}
+int main(int argc, char* argv[]) {
+    using namespace INWAD;
+    BuchiAutomaton a;
+    a.addTransition(1,"",1);
+    std::cout << a.transitions() << '\n';
+
+    BuchiAutomaton b(2, {"a", "b"}, 0);
+    b.addTransition(0,"a",1);
+    std::cout << b.transitions() << '\n';
+}
