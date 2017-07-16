11b85e7c09c42752a604be3d00fcf2d42e1331fb
Duc Thanh Tran <ducthanhtran@users.noreply.github.com>
2017-07-16 09:13:33 +0200

    formatting - made comments clearer

--- src/buchiAutomaton.cpp
+++ src/buchiAutomaton.cpp
@@ -3,7 +3,8 @@
 
 namespace INWAD {
 
-BuchiAutomaton::BuchiAutomaton(const int numberOfStates, const std::vector<std::string> &alphabet, const State initialState) : m_transitions(numberOfStates), m_alphabet(alphabet), m_initialState(initialState) {}
+BuchiAutomaton::BuchiAutomaton(const int numberOfStates, const Alphabet &alphabet, const State initialState)
+    : m_transitions(numberOfStates), m_alphabet(alphabet), m_initialState(initialState) {}
 
 int BuchiAutomaton::transitions() const noexcept {
     return std::accumulate(m_transitions.cbegin(), m_transitions.cend(), 0,
@@ -11,10 +12,10 @@ int BuchiAutomaton::transitions() const noexcept {
 }
 
 void BuchiAutomaton::addTransition(const State source, const std::string &label, const State target) {
-    // Check that label is part of the alphabet
+    // Label must be part of the alphabet
     const auto index = std::find(m_alphabet.cbegin(), m_alphabet.cend(), label);
     if (index != m_alphabet.cend()) {
-        // Make that that there is no transition to target with label
+        // Disallow duplicate transitions
         const auto range = m_transitions[source].equal_range(label);
         const auto sameTrans = std::find_if(range.first, range.second, [target](const auto &elem) { return elem.second == target; });
         if (sameTrans == m_transitions[source].cend()) {
