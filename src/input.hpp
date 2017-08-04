1ff360d5ee7c413a06f84c9b913f42908e8ccf16
Duc Thanh Tran <ducthanhtran@users.noreply.github.com>
2017-08-04 13:22:34 +0200

    documentation of read method

--- src/input.hpp
+++ src/input.hpp
@@ -13,18 +13,19 @@ namespace INWAD {
 
 //! Reads a Buchi automaton from a file.
 //!
-//! The file has a specific format. Each line's first letter denotes
+//! The file has a specific format. The first line we read denotes the amount
+//! of states in the automaton. In the second line we list the alphabet where
+//! each element is seperated by a whitespace. The initial state is mentioned
+//! in the third line. Afterwards we process in the following way:
+//!
+//! Each line's first letter denotes
 //! what type of line will be read.
 //!
 //!    'c' ...   : comment line
 //!    't' q a p : transition from state q to p with label a
 //!
-//! The first line we read denotes the amount of states in the automaton.
-//! In the second line we list the alphabet where each element is seperated
-//! by a whitespace.
-//! The initial state is mentioned in the third line
-//!
-//! If we cannot read the file we return an empty Buchi automaton.
+//! If we cannot read the file (correctly according to the format mentioned
+//! above), then an empty Buchi automaton is returned.
 BuchiAutomaton read(const std::string filepath);
 
 } // namespace INWAD
