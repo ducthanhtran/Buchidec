db39721b8b177cb823b57ca20c191e070dcc11cd
Duc <ducthanhtran@users.noreply.github.com>
2017-10-24 09:06:49 +0200

    Remove C++ and switch to Julia
    Switching to Julia; in order to learn new programming language

--- src/emptiness.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace INWAD {
-    namespace Detail {
-
-    } // namespace Detail
-
-    bool acceptsEmptyLanguage(const BuchiAutomaton &aut);
-} // namespace INWAD
--- src/uncountability.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace INWAD {
-    namespace Detail {
-
-    } // namespace Detail
-
-    bool acceptsUncountableLanguage(const BuchiAutomaton &aut);
-} // namespace INWAD
